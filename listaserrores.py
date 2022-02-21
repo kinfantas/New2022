f16ca8f6b5b9411fd6ae3fb6e7fc6ed5902c17ea
Karem Infantas <kareminfantas@gmail.com>
2022-02-21 10:33:13 -0400

    Add files via upload

---
+++ listaserrores.py
@@ -0,0 +1,14 @@
+# Recorriendo los elementos
+for edad in edades:
+    print(edad)
+
+# Recorriendo los índices
+for i in range(len(edaded)):
+    print(edaded[i])
+
+# Con while y los índices
+indice = 0
+
+while indice < len(edaded):
+    print(edades[indice])
+    indice += 1
