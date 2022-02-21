4659ede6dce5d593c4e95e77b4f920ee7269d3c4
Karem Infantas <kareminfantas@gmail.com>
2022-02-21 10:11:43 -0400

    Add files via upload

---
+++ calculardistancia.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#Decoración: Nombre del Algoritmo
+print("-------------------------------------------------------")
+print("Ejercicio2: PROMEDIO DE 3 NOTAS.")
+print("-------------------------------------------------------")
+#Entradas
+print("Ingrese las 3 notas del alumno N1 , N2, N3")
+N1 = int( input("N1: "))
+N2 = int( input("N2: "))
+N3 = int( input("N3: "))
+#Proceso
+P = int( (N1+N2+N3)/3 )
+print (P)
---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Programa que convierte ºC a ºF
+C = float(input("Entra temperatura en ºC: "))
+F = 1.8*C + 32
+print(C, "ºC equivale a", F, "ºF")
